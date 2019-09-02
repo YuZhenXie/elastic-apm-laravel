47c70a4c101ab0f9e2b790d2475fa75c1173ffe5
yuzhen.xie <yuzhen.xie@rakuten.com>
2019-09-02 15:22:35 +0900

    update agent version to 7.0.0-beta1

--- src/Providers/ElasticApmServiceProvider.php
+++ src/Providers/ElasticApmServiceProvider.php
@@ -183,7 +183,7 @@ class ElasticApmServiceProvider extends ServiceProvider
             $query = [
                 'name' => 'Eloquent Query',
                 'type' => 'db.mysql.query',
-                'timestamp' => round(microtime(true) * 1000000) - round($query->time * 1000),
+                'start' => round((microtime(true) - $query->time / 1000 - $this->startTime) * 1000, 3),
                 // calculate start time from duration
                 'duration' => round($query->time, 3),
                 'stacktrace' => $stackTrace,
